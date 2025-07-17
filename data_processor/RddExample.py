b295eb029835b1b5c50e5c4824f3d216d3c996ed
Abhishek Wasave <Abhi_wasave@abw12>
2025-07-17 23:07:43 +0530

    avg frds per age group

--- data_processor/RddExample.py
+++ data_processor/RddExample.py
@@ -1,3 +1,4 @@
+from pyspark import SparkContext
 from pyspark.sql import SparkSession
 
 
@@ -22,17 +23,36 @@ class Student:
         )
 
 
+def parseData(data_lines) -> tuple[int,int]:
+    fields =data_lines.split(',') # 4 fields are there in the csv file
+    age:int = int(fields[2]) # type caste the str to int
+    numFrds:int = int(fields[3])
+    return (age,numFrds)
+
+def calcAvgFrdsData(sc:SparkContext, filePath:str):
+    file_data = sc.textFile(filePath)  # this is another way to create an rdd from a file. it return rdd of str 
+    rdd = file_data.map(parseData) # map the data to a specific format( rdd of (age,numFrds))
+    totalsByAge = rdd.mapValues(lambda x: (x, int(1))).reduceByKey(lambda x,y: (x[0] + y[0], x[1] + y[1])) # (age = 33, numfrds = (387,2)) - suppose for age 33 we have two entries one with (385,1) and other with (2,1) => both adds up to (387,2) here 2 represent the num of occurences(count of age 33 appeared in the entire file)
+    avgByAge  = totalsByAge.mapValues(lambda x: x[0] / x[1] )
+    results = avgByAge.collect()
+    for result in results:
+        print(result)
 
 def main():
-    student_list = Student.fetchData()
+    
     spark = SparkSession.builder.appName("RddExample").config("spark.master","local").getOrCreate() #type: ignore
     sc = spark.sparkContext
-    # Create an RDD of student list
-    students_rdd = sc.parallelize(student_list)
-    # Create (name, marks) pairs and returns an RDD
-    keyvalueRdd = students_rdd.map(lambda s: (s.name,s.marks)) ## creating a (key:value) pair rdd
-    aggregated_marks = keyvalueRdd.reduceByKey(lambda a,b: a + b) #For each unique name, sums up all the marks.
-    print(aggregated_marks.collect()) 
+    
+    # student_list = Student.fetchData()
+    # # Create an RDD of student list
+    # students_rdd = sc.parallelize(student_list, numSlices=3)
+    # # Create (name, marks) pairs and returns an RDD
+    # keyvalueRdd = students_rdd.map(lambda s: (s.name,s.marks)) ## creating a (key:value) pair rdd
+    # aggregated_marks = keyvalueRdd.reduceByKey(lambda a,b: a + b) #For each unique name, sums up all the marks.
+    # print(aggregated_marks.collect()) 
+
+    # fake friends data set example of rdd 
+    calcAvgFrdsData(sc,'C:/Spark_learning/PySparkPractise/PySparkDemo/data/fakefriends.csv')
 
 
 if __name__ == "__main__":
